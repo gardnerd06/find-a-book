94d9223f314e6296b234b8af478a798600130861
gardnerd06 <115792714+gardnerd06@users.noreply.github.com>
2023-04-26 18:29:53 -0400

    added code to initialize the apollo server

---
+++ server/server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const path = require('path');
+const { ApolloServer } = require('apollo-server-express');
+const routes = require('./routes');
+const { authMiddleware } = require('./utils/auth');
+
+const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.use(routes);
+
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+// });
+// created a new instance of the apollo server with graphQL
+const startApolloServer = async () => {
+  await server.start();
+  server.applyMiddleware({ app });
+
+  db.once('open', () => {
+    app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  });
+};
+
+startApolloServer();
