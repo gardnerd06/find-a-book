50fd716b6f9d902c19174a4c81e48ccfe8141903
gardnerd06 <115792714+gardnerd06@users.noreply.github.com>
2023-05-01 23:50:08 -0400

    erased unneccesary code

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -62,39 +62,10 @@ const SearchBooks = () => {
     }
   };
 
-  // // create function to handle saving a book to our database
-  // const handleSaveBook = async (bookId) => {
-  //   // find the book in `searchedBooks` state by the matching id
-  //   const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
-
-  //   // get token
-  //   const token = Auth.loggedIn() ? Auth.getToken() : null;
-
-  //   if (!token) {
-  //     return false;
-  //   }
-
-  //   try {
-  //     const response = await saveBook(bookToSave, token);
-
-  //     if (!response.ok) {
-  //       throw new Error('something went wrong!');
-  //     }
-
-  //     // if book successfully saves to user's account, save book id to state
-  //     setSavedBookIds([...savedBookIds, bookToSave.bookId]);
-  //   } catch (err) {
-  //     console.error(err);
-  //   }
-  // };
 
   const HandleSaveBook = async (bookId) => {
-    console.log(bookId);
+
     const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
-    // const [book, setBook] = useState('');
-
-    // const token = Auth.loggedIn() ? Auth.getToken() : null;
-
     try {
       const data = await SaveBook({
         variables: { book: { ...bookToSave } },
